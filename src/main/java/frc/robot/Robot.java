be4df22c981a5869f6bee4c8054ccb413a111e65
Daniel Margavio <danmargavio@gmail.com>
2024-08-25 09:11:06 -0500

    updates

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -28,7 +28,7 @@ public class Robot extends TimedRobot {
    */
 
   // We define 3 constants so that we can easily adjust our controller gain constants
-  final double kP = 1.0;
+  final double kP = 20.0;
   final double kI = 0.0;
   final double kD = 0.001;
 
@@ -36,17 +36,25 @@ public class Robot extends TimedRobot {
   final double kPosition1 = 0.8;
   final double kPosition2 = 0.0;
 
-  // This is a variable to store our feedback data
-  double feedbackKrakenX60Position;
+  // We define two constants for the commanded velocity; this is in units of shaft rotations per minute
+  final double kVelocity1 = 10.0;
+  final double kVelocity2 = 0.0;
+
+  // These variables store our feedback data
+  double feedbackKrakenX60_Position;
+  double feedbackKrakenX60_Velocity;
 
   // We define a PID controller that we utilizes the gain constants
-  PIDController myPIDController = new PIDController(kP, kI, kD);
+  PIDController myPIDController_Position = new PIDController(kP, kI, kD);
+  PIDController myPIDController_Velocity = new PIDController(kP, kI, kD);
 
   // We define a TalonFX motor controller that can be used to move the Kraken X60 and generate position feedback data
   final TalonFX myKrakenX60 = new TalonFX(20);
 
   // The Configuration object is something the KrakenX60 requires to be configured
   final TalonFXConfiguration myKrakenX60Config = new TalonFXConfiguration();
+
+  // These VoltageOut object is needed by the KrakenX60 to control the motors
   final VoltageOut myVoltage = new VoltageOut(0.0);
 
   @Override
@@ -58,24 +66,30 @@ public class Robot extends TimedRobot {
     myKrakenX60.getConfigurator().apply(myKrakenX60Config, 1.0);
 
     // This provides an initial set point for the controller, which is the starting position or 0.0
-    myPIDController.setSetpoint(0.0);
+    myPIDController_Position.setSetpoint(0.0);
+    myPIDController_Velocity.setSetpoint(0.0);
   }
 
   @Override
   public void robotPeriodic() {
 
-    // This retrieves the rotational position of the motor shaft in # of rotations
-    feedbackKrakenX60Position = myKrakenX60.getPosition().getValueAsDouble();
+    // This retrieves the rotational position and velocity of the motor shaft in # of rotations and rotations per minute
+    feedbackKrakenX60_Position = myKrakenX60.getPosition().getValueAsDouble();
+    feedbackKrakenX60_Velocity = myKrakenX60.getVelocity().getValueAsDouble();
 
     // This method performs the controller calculations using a feedback measurement we give it and outputs a voltage to actually control the motor
-    myVoltage.withOutput(myPIDController.calculate(feedbackKrakenX60Position));
+    myVoltage.withOutput(myPIDController_Position.calculate(feedbackKrakenX60_Position));
+
+    // The version below is the same as above except it controls using velocity; uncomment to use it instead of the above
+    myVoltage.withOutput(myPIDController_Velocity.calculate(feedbackKrakenX60_Velocity));
   }
 
   @Override
   public void autonomousInit() {
 
-    // This will command the motor setpoint to position 2 whenever you switch to Autonomous; but doesn't actually command it move
-    myPIDController.setSetpoint(kPosition2);
+    // This will set the motor setpoint to position/velocity 2 whenever you switch to Autonomous; but doesn't actually command it move
+    myPIDController_Position.setSetpoint(kPosition2);
+    myPIDController_Velocity.setSetpoint(kVelocity2);
   }
 
   @Override
@@ -88,8 +102,9 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit() {
     
-    // This will command the motor setpoint to position 1 whenever you switch to TeleOp; but doesn't actually command it move
-    myPIDController.setSetpoint(kPosition1);
+    // These will set the motor setpoint to position/velocity 1 whenever you switch to TeleOp; but doesn't actually command it move
+    myPIDController_Position.setSetpoint(kPosition1);
+    myPIDController_Velocity.setSetpoint(kVelocity1);
   }
 
   @Override
